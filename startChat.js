6cc15e74cfaa79d035baf5b76232a142742b9eac
Rafael Farias <rafael.oliveirafa@gmail.com>
2021-05-19 21:34:13 -0300

    Foram realizadas alterações para tornar o código mais limpo como também foi incluída a lógica para atualizar uma aba existente do WhatsApp ao invés de abrir uma nova

--- startChat.js
+++ startChat.js
@@ -1,25 +1,37 @@
- var contextMenuItem = {
+var contextMenuItem = {
     "id": "Chat",
     "title": "Iniciar conversa",
     "contexts": ["selection"]
 };
+/**
+ * 
+ * @param {*} selectedData 
+ */
+function newChat(selectedData) {
 
-function newChat(clickData) {
+    var telephoneNumber = selectedData.selectionText.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '');
+    var splitedTelephoneNumber = telephoneNumber.split(/^(\d{1,2})(\d{1,3})(\d{4,10})$/);
 
-    var varNumber = clickData.selectionText.normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '');
-    var splitedNumber = varNumber.split(/^(\d{1,2})(\d{1,3})(\d{4,10})$/);
+    if (splitedTelephoneNumber[1] != "55") {
+        telephoneNumber = "55".concat(telephoneNumber);
+    }
 
-    if(splitedNumber[1] != "55")
-        varNumber = "55".concat(varNumber);
-    
-    if(/^[0-9]?()[0-9](\s|\S)(\d[0-9]{9})$/gm.test(varNumber) || /^[0-9]?()[0-9](\s|\S)(\d[0-9]{10})$/gm.test(varNumber)) 
-        chrome.tabs.create({"url" : ("https://web.whatsapp.com/send?phone="+varNumber)});
+    if (/^[0-9]?()[0-9](\s|\S)(\d[0-9]{9})$/gm.test(telephoneNumber) || /^[0-9]?()[0-9](\s|\S)(\d[0-9]{10})$/gm.test(telephoneNumber)) {
+        chrome.tabs.query({ url: 'https://web.whatsapp.com/' }, function (tabs) {
+            if (Array.isArray(tabs) && tabs.length !== 0) {
+                var tabId = tabs[0].id;
+                chrome.tabs.update(tabId, {
+                    url: 'https://web.whatsapp.com/send?phone=' + telephoneNumber
+                });
+            } else {
+                chrome.tabs.create({ url: 'https://web.whatsapp.com/send?phone=' + telephoneNumber });
+            }
+        });
+    }
 };
 
 chrome.contextMenus.create(contextMenuItem);
 
-chrome.contextMenus.onClicked.addListener(function(clickData){
-
-    newChat(clickData);
-    
+chrome.contextMenus.onClicked.addListener(function (selectedData) {
+    newChat(selectedData);
 });
